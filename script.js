1b9c7c302cd158a1bdf583eb2a2cdebc16442659
Alan Ferreira <alanferreiraapps@gmail.com>
2022-01-19 16:01:48 -0300

    rodapé

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ document.querySelector(".hamburguer").addEventListener("click", () =>
     document.querySelector(".container").classList.toggle("show-menu")
 );
 
+document.querySelector("#qtde").addEventListener("change", atualizarPreco)
+
+function atualizarPreco(){
+    const qtde = document.querySelector("#qtde").value
+    let preco = qtde * 170;
+    document.querySelector("#preco").innerHTML = 'R$ {preco}'
+    console.log(preco)
+}
+
+
