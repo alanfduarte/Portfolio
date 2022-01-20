37d4b13248baa09e4430f4358ba20f59f3ab22c0
Alan Ferreira <alanferreiraapps@gmail.com>
2022-01-20 07:46:30 -0300

    calculo orçamento

--- script.js
+++ script.js
@@ -3,12 +3,30 @@ document.querySelector(".hamburguer").addEventListener("click", () =>
 );
 
 document.querySelector("#qtde").addEventListener("change", atualizarPreco)
+document.querySelector("#js").addEventListener("change", atualizarPreco)
+document.querySelector("#layout-sim").addEventListener("change", atualizarPreco)
+document.querySelector("#layout-nao").addEventListener("change", atualizarPreco)
+document.querySelector("#prazo").addEventListener("change", function (){
+    const prazo = document.querySelector("#prazo").value
+    document.querySelector("label[for=prazo]").innerHTML = `prazo: ${prazo} semanas`
+    atualizarPreco()
+})
+
 
 function atualizarPreco(){
     const qtde = document.querySelector("#qtde").value
+    const temJS = document.querySelector("#js").checked
+    const incluiLayout = document.querySelector("#layout-sim").checked
+    const prazo = document.querySelector("#prazo").value
+
     let preco = qtde * 170;
-    document.querySelector("#preco").innerHTML = 'R$ {preco}'
-    console.log(preco)
+    if (temJS) preco *= 1.1
+    if (incluiLayout) preco += 700
+    let taxaUrgencia = 1 - prazo * 0.1;
+    preco *= 1 + taxaUrgencia
+
+    document.querySelector("#preco").innerHTML = `R$ ${preco.toFixed(2)}`
+    
 }
 
 
